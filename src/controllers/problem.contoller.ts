2af9f4d59ee056ed8db714f899dcc1068501364e
berasourabh11 <berasourabh2071@gmail.com>
2024-03-20 21:41:39 +0530

    added not implemented functions

--- src/controllers/problem.contoller.ts
+++ src/controllers/problem.contoller.ts
@@ -1,36 +1,34 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
+import StatusCodes from "http-status-codes"
 
-function pingProblemController(req:Request, res:Response) {
-  return res.json({message:"Problem service is alive"});
+function pingProblemController(req: Request, res: Response) {
+  return res.json({ message: "Pong from problem service" });
 }
 
-function addProblem(req:Request, res:Response){
-
+function addProblem(req: Request, res: Response) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
 }
 
-function getProblems(req:Request, res:Response){
-
+function getProblems(req: Request, res: Response) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
 }
-function getProblem(req:Request, res:Response){
-
-}
-
-
-function deleteProblem(req:Request, res:Response){
-
+function getProblem(req: Request, res: Response) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
 }
 
-function updateProblem(req:Request, res:Response){
-
+function deleteProblem(req: Request, res: Response) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
 }
 
-
+function updateProblem(req: Request, res: Response) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
+}
 
 export {
-    pingProblemController,
-    addProblem,
-    getProblems,
-    getProblem,
-    deleteProblem,
-    updateProblem
-}
+  pingProblemController,
+  addProblem,
+  getProblems,
+  getProblem,
+  deleteProblem,
+  updateProblem,
+};
