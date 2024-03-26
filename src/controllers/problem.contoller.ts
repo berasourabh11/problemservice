5a3a8c9e173ecf67db6bab4e9bdf408d9a258ff1
berasourabh11 <berasourabh2071@gmail.com>
2024-03-26 20:09:47 +0530

    added error handlers

--- src/controllers/problem.contoller.ts
+++ src/controllers/problem.contoller.ts
@@ -1,27 +1,48 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import StatusCodes from "http-status-codes"
+import { NotImplemented } from "../errors/notImplemented.error";
 
 function pingProblemController(req: Request, res: Response) {
   return res.json({ message: "Pong from problem service" });
 }
 
-function addProblem(req: Request, res: Response) {
-  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
+function addProblem(req: Request, res: Response,next:NextFunction) {
+  try{
+    throw new NotImplemented("add problem")
+  } catch (error) {
+    next(error)
+  }
 }
 
-function getProblems(req: Request, res: Response) {
-  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
+function getProblems(req: Request, res: Response,next:NextFunction) {
+  try{
+    throw new NotImplemented("add problem")
+  } catch (error) {
+    next(error)
+  }
 }
-function getProblem(req: Request, res: Response) {
-  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
+function getProblem(req: Request, res: Response,next:NextFunction) {
+    try{
+    throw new NotImplemented("add problem")
+  } catch (error) {
+    next(error)
+  }
 }
 
-function deleteProblem(req: Request, res: Response) {
-  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
+function deleteProblem(req: Request, res: Response,next:NextFunction) {
+    try{
+    throw new NotImplemented("add problem")
+  } catch (error) {
+    next(error)
+  }
 }
 
-function updateProblem(req: Request, res: Response) {
-  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
+function updateProblem(req: Request, res: Response,next:NextFunction) {
+    try{
+    throw new NotImplemented("add problem")
+  } catch (error) {
+    next(error)
+  }
 }
 
 export {
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ const app = express();
 import { PORT } from './config/server.config';
 import bodyParser from 'body-parser';
 import { apiRouter } from './routes';
+import errorHandler from './utils/errorHandler';
 
 
 app.use(bodyParser.json());
@@ -15,6 +16,8 @@ app.get("/ping",(req,res)=>{
   return res.json({message:"Problem service is alive"});
 })
 
+app.use(errorHandler);
+
 app.listen(PORT,()=>{
-  console.log("Problem service listning on",PORT)
+  console.log("Problem service listning on",PORT);
 })
