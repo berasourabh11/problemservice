d4db004b6b12129156002beba5760dcbf558ff5f
berasourabh11 <berasourabh2071@gmail.com>
2024-04-05 23:24:48 +0530

    Feat:Added all routes in place

--- src/controllers/problem.contoller.ts
+++ src/controllers/problem.contoller.ts
@@ -1,77 +1,94 @@
 import { NextFunction, Request, Response } from "express";
-import StatusCodes from "http-status-codes"
+import StatusCodes from "http-status-codes";
 import { NotImplemented } from "../errors/notImplemented.error";
 import { ProblemService } from "../services/";
 import { ProblemRepository } from "../repositories";
 
-const problemService= new ProblemService(new ProblemRepository());
-
+const problemService = new ProblemService(new ProblemRepository());
 
 function pingProblemController(req: Request, res: Response) {
   return res.json({ message: "Pong from problem service" });
 }
 
-async function addProblem(req: Request, res: Response,next:NextFunction) {
-  try{
+async function addProblem(req: Request, res: Response, next: NextFunction) {
+  try {
     const newProblem = await problemService.createProblem(req.body);
     return res.status(StatusCodes.CREATED).json({
-      success:true,
-      message:"Problem created successfully",
-      error:{},
-      data:{
-        problem:newProblem
-      }
+      success: true,
+      message: "Problem created successfully",
+      error: {},
+      data: {
+        problem: newProblem,
+      },
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
 
-async function getProblems(req: Request, res: Response,next:NextFunction) {
-  try{
+async function getProblems(req: Request, res: Response, next: NextFunction) {
+  try {
     const problems = await problemService.getProblems();
     return res.status(StatusCodes.OK).json({
-      success:true,
-      message:"Problems fetched successfully",
-      error:{},
-      data:{
-        problems:problems
-      }
+      success: true,
+      message: "Problems fetched successfully",
+      error: {},
+      data: {
+        problems: problems,
+      },
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
-async function getProblem(req: Request, res: Response,next:NextFunction) {
-    try{
-    const problemID= req.params.id;
+async function getProblem(req: Request, res: Response, next: NextFunction) {
+  try {
+    const problemID = req.params.id;
     const problem = await problemService.getProblem(problemID);
     return res.status(StatusCodes.OK).json({
-      success:true,
-      message:"Problem fetched successfully",
-      error:{},
-      data:{
-        problem:problem
-      }
+      success: true,
+      message: "Problem fetched successfully",
+      error: {},
+      data: {
+        problem: problem,
+      },
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
 
-function deleteProblem(req: Request, res: Response,next:NextFunction) {
-    try{
-    throw new NotImplemented("add problem")
+async function deleteProblem(req: Request, res: Response, next: NextFunction) {
+  try {
+    const problemID = req.params.id;
+    const problem = await problemService.deleteProblem(problemID);
+    return res.status(StatusCodes.OK).json({
+      success: true,
+      message: "Problem deleted successfully",
+      error: {},
+      data: {
+        problem: problem,
+      },
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
 
-function updateProblem(req: Request, res: Response,next:NextFunction) {
-    try{
-    throw new NotImplemented("add problem")
+async function updateProblem(req: Request, res: Response, next: NextFunction) {
+  try {
+    const problemID = req.params.id;
+    const updatedProblem = await problemService.updateProblem(problemID, req.body);
+    return res.status(StatusCodes.OK).json({
+      success: true,
+      message: "Problem updated successfully",
+      error: {},
+      data: {
+        problem: updatedProblem,
+      },
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
 
